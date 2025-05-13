f6ef2505f5481a9985f18bd64775de3764c08ef7
Kaori Kawakami <k.kawakami08@gmail.com>
2025-05-12 18:10:22 -0700

    updated import

---
+++ src/components/shared/header/search.tsx
@@ -0,0 +1,10 @@
+// import { getAllCategories } from "@/lib/actions/product.actions";
+import React from "react";
+
+const Search = async () => {
+  // const categories = await getAllCategories()
+
+  return <div>Search</div>;
+};
+
+export default Search;
